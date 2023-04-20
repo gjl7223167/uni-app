a4db8737f7f9d863e23df2b5cc4cd7f91a23471c
gjl7223167 <gjl7223167@163.com>
2023-04-20 17:42:09 +0800

    更新

--- HBuilderProjects/uniApp-Sample/pages/index/index.vue
+++ HBuilderProjects/uniApp-Sample/pages/index/index.vue
@@ -1,37 +1,52 @@
 <template>
 	<view class="content">
 		<form action="" @submit="submitForm" @reset="resetForm">
-			<view>
-				<swiper checked @click="swiperChagee" name="swiperValue"></swiper>
+			<view class="uni-list">
+				<view class="uni-list-cell-pd uni-list-cell">
+					<view class="uni-list-cell-db">开启中</view>
+					<switch checked @click="swiperChagee" name="swiperValue"></switch>
+				</view>
 			</view>
-			<view>
-				<radio-group name="selType">
-					<label name="selType">
+			<view class="row">
+				<radio-group name="selType" class="v-radio">
+					<label name="selType" class="v-row">
 						<radio value="选项一"></radio><text>选项一</text>
 					</label>
-					<label name="selType">
+					<label name="selType" class="v-row">
 						<radio value="选项二"></radio><text>选项一</text>
 					</label>
-					<label name="selType">
+					<label name="selType" class="v-row">
 						<radio checked value="其他"></radio><text>其他选项</text>
 					</label>
 				</radio-group>
 			</view>
-			
 			<view>
-				<input class="uni-input" name="input" placeholder="这是一个输入框" />
-
+				<picker :range="options" mode="selector" name="school" :value="selectValue" @change="pickChange">
+					<label class="barder_row">点击请选择学历:{{options[selectValue]}}</label>
+				</picker>
 			</view>
-			
+
+			<view class="table_row">
+				<input class="uni-input" name="input" placeholder="请输入" />
+			</view>
+
 			<view>
 				<textarea placeholder="请输入内容" name="textFinle"></textarea>
 			</view>
-			
+
+
 			<view>
-				<button  class="formBtn" form-type="submit">提交</button>
-				<button  class="formBtn" form-type="reset">重置</button>
+				<button class="formBtn" form-type="submit">提交</button>
+				<button class="formBtn" form-type="reset">重置</button>
 			</view>
+			{{obj}}
 		</form>
+	
+	<input type="text" v-model="title" />
+	<view>原标题: {{title}}</view>
+	<view>修改后: {{changeTitle}}</view>
+	
+	<view>{{demo}}</view>
 	</view>
 </template>
 
@@ -39,34 +54,52 @@
 	export default {
 		data() {
 			return {
+				obj: null,
+				options: ["大专", "本科", "研究生", "博士"],
+				selectValue: 2,
 				
+				title : "",
+				text: "你是谁"
 			}
 		},
 		onLoad() {
 
 		},
-		methods: {
-			submitForm(e){
-				console.log(e.detail),
-				console.log(this.radio1),
-				console.log(this.radio2)
+		computed:{
+			demo(){
+				return "uniapp" + this.text
 			},
-			resetForm(){
+			changeTitle(){
+			 	return this.title.toLocaleUpperCase()
+			}
+		},
+
+		methods: {
+			submitForm(e) {
+				console.log(e.detail)
+				this.obj = e.detail.value
+				this.obj.school = this.options[this.selectValue]
+			},
+			resetForm() {
 				console.log('清空数据')
 			},
-			swiperChagee(e){
-				
+			swiperChagee(e) {
+
+			},
+			pickChange(e) {
+				this.selectValue = e.detail.value
 			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+		margin: 10px;
 	}
 
 	.logo {
@@ -87,13 +120,79 @@
 		font-size: 36rpx;
 		color: #8f8f94;
 	}
-	.formBtn{
+
+	.formBtn {
 		align-content: center;
 		width: 200rpx;
 		border-style: solid;
 	}
-		
-	.uni-input{
-		border-style: solid;
+
+	.table_row {
+		margin-top: 10px;
+		padding: 6px;
+	}
+
+	input,
+	textarea {
+		border: solid 1px #ccc;
+		margin-top: 10px;
+		border-radius: 5px;
+		padding: 6px;
+	}
+
+	.barder_row {
+		border: solid 2px #ccc;
+		border-radius: 5px;
+		background-color: #8f8f94;
+		padding: 6px;
+	}
+
+	.row {
+		margin-bottom: 10px;
+
+		.v-radio {
+			display: grid;
+			margin-bottom: 10rpx;
+
+			.v-row {
+				margin-bottom: 10px;
+			}
+		}
+	}
+
+	.uni-list {
+		display: flex;
+		flex-direction: column;
+		position: relative;
+	}
+
+	.uni-list-cell-db {
+		flex: 1;
+	}
+
+	.uni-list-cell {
+		position: relative;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+	
+	.uni-list-cell-pd {
+		padding: 22rpx 30rpx;
+	}
+
+	.uni-list-cell::after {
+		display: flex;
+		position: absolute;
+		z-index: 3;
+		right: 0;
+		bottom: 0;
+		left: 30rpx;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
 	}
 </style>
